558839404689888923d628b6d6f4c5ae2d075b65
Andrej Kvasnica <andrej@gmail.com>
2018-04-18 09:49:45 +0200

    long lines short

--- vltava.sh
+++ vltava.sh
@@ -17,15 +17,42 @@ TMPFILE=$(mktemp) || { echo "Failed to create temp file"; exit 1; }
 
 wget -q $1 -O $TMPFILE
 
-FILENAME=$(cat $TMPFILE | grep og:title | sed -e "s/.*content=\"//g" -e "s/\" .*//g" -e "s/\. *\(.*\)/ (\1)/g" | tr -d "\"" | tr ":\?\!/" "----" | sed -e "s/-/ - /g" -e "s/  / /g" -e "s/ $//g" -e "s/ /\\ /g")
-ID=$(cat $TMPFILE | grep filename | grep -v rights-expired | sed -e "s/.*a href=\"//g" -e "s/\?uuid.*//g")
+FILENAME=$(\
+    cat $TMPFILE \
+    | grep og:title \
+    | sed -e "s/.*content=\"//g" -e "s/\" .*//g" -e "s/\. *\(.*\)/ (\1)/g" \
+    | tr -d "\"" \
+    | tr ":\?\!/" "----" \
+    | sed -e "s/-/ - /g" -e "s/  / /g" -e "s/ $//g" -e "s/ /\\ /g"\
+)
+ID=$(
+    cat $TMPFILE \
+    | grep filename \
+    | grep -v rights-expired \
+    | sed -e "s/.*a href=\"//g" -e "s/\?uuid.*//g"\
+)
 
 IDS=(${ID// / })
 RIADKOV=${#IDS[@]}
 
 if [[ $RIADKOV -eq 0 ]]; then
-    FILENAME=$(cat $TMPFILE | grep "><h1>" | grep image | grep title | sed -e "s/.*<h1>//g" -e "s/<\/h1>.*//g" | tr -d "\"" | tr ":\?\!/" "----" | sed -e "s/-/ - /g" -e "s/  / /g" -e "s/ $//g" -e "s/ /\\ /g" | head -n 1)
-    ID=$(cat $TMPFILE | grep player-archive | sed -e "s/.*a><a href=\"http:\/\/prehravac.rozhlas.cz\/audio\//http:\/\/media.rozhlas.cz\/_audio\//g" -e "s/\" title.*//g" | head -n 1)
+    FILENAME=$(\
+        cat $TMPFILE \
+        | grep "><h1>" \
+        | grep image \
+        | grep title \
+        | sed -e "s/.*<h1>//g" -e "s/<\/h1>.*//g" \
+        | tr -d "\"" \
+        | tr ":\?\!/" "----" \
+        | sed -e "s/-/ - /g" -e "s/  / /g" -e "s/ $//g" -e "s/ /\\ /g" \
+        | head -n 1\
+    )
+    ID=$(\
+        cat $TMPFILE \
+        | grep player-archive \
+        | sed -e "s/.*a><a href=\"http:\/\/prehravac.rozhlas.cz\/audio\//http:\/\/media.rozhlas.cz\/_audio\//g" -e "s/\" title.*//g" \
+        | head -n 1\
+    )
     # echo "Filename: $FILENAME"
     echo "ID/URL: $ID"
     wget $WGET_PARAMS -q $ID -O "$FILENAME.mp3" && echo "$FILENAME.mp3 OK" || echo "$FILENAME.mp3 ERROR"
@@ -35,7 +62,15 @@ elif [[ $RIADKOV -eq 1 ]]; then
     wget $WGET_PARAMS -q $ID -O "$FILENAME.mp3" && echo "$FILENAME.mp3 OK" || echo "$FILENAME.mp3 ERROR"
 else
     echo $RIADKOV zaznamov
-    TITLE_ALL=$(cat $TMPFILE | grep filename | grep -v rights-expired | sed -e "s/.*title=\"//g" -e "s/\">.*//g" | tr -d "\"" | tr ":\?\!/" "----" | sed -e "s/-/ - /g" -e "s/  / /g" -e "s/ $//g" -e "s/ /\\ /g")
+    TITLE_ALL=$(\
+        cat $TMPFILE \
+        | grep filename \
+        | grep -v rights-expired \
+        | sed -e "s/.*title=\"//g" -e "s/\">.*//g" \
+        | tr -d "\"" \
+        | tr ":\?\!/" "----" \
+        | sed -e "s/-/ - /g" -e "s/  / /g" -e "s/ $//g" -e "s/ /\\ /g"\
+    )
     IFS=$'\n' TITLES=(${TITLE_ALL})
 
     for INDEX in "${!IDS[@]}"
